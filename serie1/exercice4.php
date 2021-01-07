7980c56b020965ff7f75e4d1f9861b1aefb0abe2
arthurmadecprevost <arthurmadec@orange.fr>
2021-01-07 09:40:51 +0100

    modifications mines

--- serie1/exercice4.php
+++ serie1/exercice4.php
@@ -35,49 +35,43 @@
             Jeu du démineur
           </div>
           <div class="card-body">
+            <div class="table-responsive-xl">
+                <table class="table">
+                  <tbody>
+                  <tr>
+                  <?php
+                    $nbLigne = 15;
+                    $nbColonne = 15;
+                    $nbCases = $nbLigne*$nbColonne;
+                    $nbMine = 5;
+
+                    $mines = array();
+                    for($i = 1;$i <= $nbMine;$i++)
+                    {
+                      array_push($mines, random_int(0,$nbCases));
+                    }
+                    echo("Tableau des emplacements des mines:<br>");
+                    print_r($mines);
+                    echo("<br>");
+
+                    for($j = 1; $j <= $nbCases; $j++){
+                      if(is_int($j/$nbColonne+1)){
+                        echo("</tr><tr>");
+                      } else {
+                        if(in_array($j, $mines)){
+                          echo("<td><b>X</b></td>");
+                        } else {
+                          echo("<td>o</td>");
+                        }
+                      }
+                    }
+                  ?>  
+                  </tbody>
+                </table>
+              </div>
             <p class="card-text">
-            <?php
-        $nbLigne = 15;
-        $nbColonne = 50;
-        $nbCases = $nbLigne*$nbColonne;
-        $nbMine = 5;
-
-        $mines = array();
-        for($i = 1;$i <= $nbMine;$i++)
-        {
-          array_push($mines, random_int(0,$nbCases));
-        }
-        print_r($mines);
-        echo("<br>");
-        $nbRandomLigne = random_int(0,$nbLigne);
-        $nbRandomColonne = random_int(0,$nbColonne);
-
-        for($j = 0; $j <= $nbCases; $j++){
-          if(in_array($j, $mines)){
-            echo("*");
-          } else {
-            echo(".");
-          }
-        }
-        //for()
-        //in_array($j, $mines);
-        /*for($ligne = 0;$ligne < $nbLigne;$ligne++)
-        {
-            for($colonne = 0;$colonne < $nbColonne;$colonne++)
-            {
-                if($colonne == $nbRandomColonne AND $ligne == $nbRandomLigne)
-                {
-                    echo " <b>*</b> ";
-                }
-                else
-                {
-                    echo " . ";
-                }
-            }
-            echo "<br>";
-        }*/
-?>  
             </p>
+            
           </div>
         </div>
 
