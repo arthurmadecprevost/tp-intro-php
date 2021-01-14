0eb1983079981fdb5b81f6934524a7155906c4fe
arthurmadecprevost <arthurmadec@orange.fr>
2021-01-14 09:16:36 +0100

    modif insert

--- serie4/insert.php
+++ serie4/insert.php
@@ -4,17 +4,17 @@
     
     if(isset($_POST['insert']))
     {
-        // récupérer les valeurs 
         $firstname = $_POST['first_name'];
         $lastname = $_POST['last_name'];
         $email = $_POST['email'];
         $contactnumber = $_POST['contactnumber'];
         $address = $_POST['address'];
-        // Requête mysql pour insérer des données
+
         $sql = "INSERT INTO `users`(`FirstName`, `LastName`, `EmailId`, `ContactNumber`, `Address`) VALUES (:firstname,:lastname,:emailId,:contactNumber,:adresse)";
+        
         $res = $conn->prepare($sql);
         $exec = $res->execute(array(":firstname"=>$firstname,":lastname"=>$lastname,":emailId"=>$email,":contactNumber"=>$contactnumber,":adresse"=>$address));
-        // vérifier si la requête d'insertion a réussi
+        
         if($exec) 
         {
             echo "<meta http-equiv=\"refresh\" content=\"0;url=lister.php\">";
@@ -23,7 +23,11 @@
         {
             echo "<div class=\"alert alert-danger\" role=\"alert\">
             Échec de l'opération d'insertion
-          </div>";
+            </div>";
         }       
     }
 ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+</body>
+</html>
