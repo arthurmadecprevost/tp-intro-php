c3003bb1acae18d2c87e6ad38406cda5f8041387
arthurmadecprevost <arthurmadec@orange.fr>
2021-01-14 08:18:13 +0100

    added insert

--- serie4/ajouter.php
+++ serie4/ajouter.php
@@ -15,16 +15,38 @@
             </div>
             <div class="col-sm">
             </div>
-            <form action="insert.php" method="post">
-              <p>Nom<input type="text" name="LastName"></p>
-              <p>Prénom<input type="text" name="FirstName"></p>
-              <p>Email<input type="text" name="Email"></p>
-              <p>Numéro de téléphone<input type="text" name="ContactNumber"></p>
-              <p>Adresse<input type="text" name="Address"></p>
-              <p><input type="submit" name="insert" value="Insérer"></p>
-            </form>
         </div>
-        
+        <h3>Formulaire d'inscription</h3>
+        <p>Merci d'avoir choisi de vous joindre au club des MIAGISTES. Pour s'inscrire, veuillez remplir le formulaire suivant</p> 
+        <form action="insert.php" method="post">
+            <div class="row">
+                <div class="col">
+                    <label>Prénom</label>
+                    <input type="text" class="form-control" name="first_name" placeholder="Prénom" required>
+                </div>
+                <div class="col">
+                    <label>Nom</label>
+                    <input type="text" class="form-control" name="last_name" placeholder="Nom" required>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <label>Email</label>
+                    <input type="text" class="form-control" name="email" placeholder="Prénom" required>
+                </div>
+                <div class="col">
+                    <label>Numéro de téléphone</label>
+                    <input type="text" class="form-control" name="contactnumber" placeholder="Nom" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Adresse</label>
+                <textarea class="form-control" name="address" rows="3"></textarea>
+            </div>
+            <br>
+            <button type="reset" class="btn btn-primary">Rénitialiser</button>
+            <button type="submit" name="insert" class="btn btn-primary">Envoyer</button>
+        </form>        
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
---
+++ serie4/insert.php
@@ -0,0 +1,27 @@
+<?php
+    include('../include/config.php');
+    include('../template/header.php');
+    
+    if(isset($_POST['insert']))
+    {
+        // récupérer les valeurs 
+        $firstname = $_POST['first_name'];
+        $lastname = $_POST['last_name'];
+        $email = $_POST['email'];
+        $contactnumber = $_POST['contactnumber'];
+        $address = $_POST['address'];
+        // Requête mysql pour insérer des données
+        $sql = "INSERT INTO `users`(`FirstName`, `LastName`, `EmailId`, `ContactNumber`, `Address`) VALUES (:firstname,:lastname,:emailId,:contactNumber,:adresse)";
+        $res = $conn->prepare($sql);
+        $exec = $res->execute(array(":firstname"=>$firstname,":lastname"=>$lastname,":emailId"=>$email,":contactNumber"=>$contactnumber,":adresse"=>$address));
+        // vérifier si la requête d'insertion a réussi
+        if($exec) 
+        {
+            echo "<meta http-equiv=\"refresh\" content=\"0;url=$URL_base/serie4/lister.php\">";
+        } 
+        else 
+        {
+            echo "Échec de l'opération d'insertion";
+        }       
+    }
+?>
